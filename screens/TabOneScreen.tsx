c95d5a678bbfba0a903df8b8f5a9812c725e2992
Yash Suresh <psyys5@nottingham.ac.uk>
2022-01-21 17:55:52 +0000

    removed 'edit screen info'

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,19 +1,21 @@
 import { StyleSheet } from 'react-native';
+//stylesheet imported from library. Allows us to create our custom stylesheet.
 
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Hello Yash</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      
     </View>
   );
 }
 
+
+//create your own styling by importing Stylesheet object
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -23,10 +25,16 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    marginBottom: 50
   },
   separator: {
     marginVertical: 30,
-    height: 1,
+    height: 5,
     width: '80%',
+    marginBottom: 50
   },
+  button: {
+
+
+  }
 });
